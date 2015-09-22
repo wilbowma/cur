722ac7d68f6a0f8da32102cd2f1fb19e896b8b14
William J. Bowman <wjb@williamjbowman.com>
2015-09-22 15:30:00 -0400

    Added missing dependency

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "cur")
 (define deps '("base" "rackunit-lib" ("redex-lib" #:version "1.6")))
-(define build-deps '("scribble-lib" "racket-doc"))
+(define build-deps '("scribble-lib" "racket-doc" "sandbox-lib"))
 (define scribblings '(("scribblings/cur.scrbl" (multi-page))))
 (define pkg-desc "Dependent types with parenthesis and meta-programming.")
 (define version "0.1")
