ac081577a0a38f4e68860035032778a18bbe26c0
William J. Bowman <wjb@williamjbowman.com>
2015-09-23 18:11:08 -0400

    Fixed scribbled inline TODOs

--- scribblings/defs.rkt
+++ scribblings/defs.rkt
@@ -4,7 +4,7 @@
 (provide (all-defined-out))
 
 (define (todo . ls)
-  (apply margin-note "TODO: " ls))
+  (apply margin-note* "TODO: " ls))
 
 (define (gtech . x)
   (apply tech x #:doc '(lib "scribblings/guide/guide.scrbl")))
