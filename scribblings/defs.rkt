31f4673477eabe357713f8f95f64c7e4faf5c510
William J. Bowman <wjb@williamjbowman.com>
2015-09-22 23:25:33 -0400

    Added missing scribble aux defs

---
+++ scribblings/defs.rkt
@@ -0,0 +1,10 @@
+#lang racket/base
+
+(require scribble/base scribble/manual)
+(provide (all-defined-out))
+
+(define (todo . ls)
+  (apply margin-note "TODO" ls))
+
+(define (gtech . x)
+  (apply tech x #:doc '(lib "scribblings/guide/guide.scrbl")))
