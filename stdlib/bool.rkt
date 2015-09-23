c5eb2ff2afc8aba1aa9b6f9ea84af17c4f66c0b9
William J. Bowman <wjb@williamjbowman.com>
2015-09-22 23:32:57 -0400

    Add more boolean stuff

--- stdlib/bool.rkt
+++ stdlib/bool.rkt
@@ -20,3 +20,25 @@
   (require rackunit)
   (check-equal? (not true) false)
   (check-equal? (not false) true))
+
+(define (and (x : Bool) (y : Bool))
+  (if x
+      y
+      (not y)))
+
+(module+ test
+  (check-equal? (and true false) false)
+  (check-equal? (and false false) true)
+  (check-equal? (and false true) false)
+  (check-equal? (and true true) true))
+
+(define (or (x : Bool) (y : Bool))
+  (if x
+      true
+      y))
+
+(module+ test
+  (check-equal? (or true false) true)
+  (check-equal? (or false false) false)
+  (check-equal? (or false true) true)
+  (check-equal? (or true true) true))
