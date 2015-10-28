7624090e5a82bdbc6dd050eef17d71d0edace618
William J. Bowman <wjb@williamjbowman.com>
2015-10-28 15:50:14 -0400

    Updated require Redex version

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "cur")
-(define deps '("base" "rackunit-lib" ("redex-lib" #:version "1.8" #;redex-1-paul-public)))
+(define deps '("base" "rackunit-lib" ("redex-lib" #:version "1.10")))
 (define build-deps '("scribble-lib" "racket-doc" "sandbox-lib"))
 (define scribblings '(("scribblings/cur.scrbl" (multi-page))))
 (define pkg-desc "Dependent types with parenthesis and meta-programming.")
