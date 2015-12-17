c978b2e45e6aeee9dfcaecaca6d4a611c39fd826
William J. Bowman <wjb@williamjbowman.com>
2015-12-17 16:02:13 -0500

    Bumped Redex version, trying to solve #30

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "cur")
-(define deps '("base" "rackunit-lib" ("redex-lib" #:version "1.10")))
+(define deps '("base" "rackunit-lib" ("redex-lib" #:version "1.11")))
 (define build-deps '("scribble-lib" "racket-doc" "sandbox-lib"))
 (define scribblings '(("scribblings/cur.scrbl" (multi-page))))
 (define pkg-desc "Dependent types with parenthesis and meta-programming.")
