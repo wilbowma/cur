fd2c5647b4d2cbb017f938a6b6b97b041804d3f2
William J. Bowman <wjb@williamjbowman.com>
2016-01-13 20:59:22 -0500

    Pulled list tests out into cur-test

--- cur-lib/cur/stdlib/list.rkt
+++ cur-lib/cur/stdlib/list.rkt
@@ -1,42 +1,20 @@
 #lang s-exp "../cur.rkt"
 (require
- "bool.rkt"
  "nat.rkt"
  "maybe.rkt"
  "sugar.rkt")
+
+(provide
+ List
+ nil
+ cons
+ list-ref)
 
 (data List : (forall (A : Type) Type)
   (nil : (forall (A : Type) (List A)))
   (cons : (forall* (A : Type) (->* A (List A) (List A)))))
 
-(module+ test
-  (require rackunit)
-  (check-equal?
-   nil
-   nil)
-  (:: (cons Bool true (nil Bool)) (List Bool))
-  (:: (lambda* (A : Type) (a : A)
-               (ih-a : (-> Nat (Maybe A))) 
-               (n : Nat)
-        (match n
-          [z (some A a)]
-          [(s (n-1 : Nat))
-           (ih-a n-1)]))
-      (forall* (A : Type) (a : A) (ih-a : (-> Nat (Maybe A)))
-               (n : Nat)
-               (Maybe A)))
-  (:: (lambda* (A : Type) (n : Nat) (none A)) (forall (A : Type) (-> Nat (Maybe A))))
-  (:: (elim List Type (lambda* (A : Type) (ls : (List A)) Nat)
-            (lambda (A : Type) z)
-            (lambda* (A : Type) (a : A) (ls : (List A)) (ih : Nat)
-                     z)
-            Bool
-            (nil Bool))
-      Nat)
-  )
-
-(define (list-ref (A : Type) (ls : (List A)))
-  ;; TODO: case* would not type-check when used. Investigate/provide better errors for case*
+(define list-ref
   (elim
    List
    Type
@@ -48,12 +26,4 @@
               (match n
                 [z (some A a)]
                 [(s (n-1 : Nat))
-                 (ih n-1)])))
-   A
-   ls))
-
-(module+ test
-  (:: list-ref (forall (A : Type) (->* (List A) Nat (Maybe A))))
-  (check-equal?
-   ((list-ref Bool (cons Bool true (nil Bool))) z)
-   (some Bool true)))
+                 (ih n-1)])))))
---
+++ cur-test/cur/tests/stdlib/list.rkt
@@ -0,0 +1,49 @@
+#lang cur
+(require
+ rackunit
+ cur/stdlib/sugar
+ cur/stdlib/bool
+ cur/stdlib/nat
+ cur/stdlib/maybe
+ cur/stdlib/list)
+
+(check-equal?
+ nil
+ nil)
+;; NB HACK: Hack to register :: as a test-case.
+;; TODO: Abstract this away
+(check-equal?
+ (void)
+ (:: (cons Bool true (nil Bool)) (List Bool)))
+(check-equal?
+ (void)
+ (:: (lambda* (A : Type) (a : A)
+             (ih-a : (-> Nat (Maybe A))) 
+             (n : Nat)
+             (match n
+               [z (some A a)]
+               [(s (n-1 : Nat))
+                (ih-a n-1)]))
+    (forall* (A : Type) (a : A) (ih-a : (-> Nat (Maybe A)))
+             (n : Nat)
+             (Maybe A))))
+(check-equal?
+ (void)
+ (:: (lambda* (A : Type) (n : Nat) (none A)) (forall (A : Type) (-> Nat (Maybe A)))))
+(check-equal?
+ (void)
+ (:: (elim List Type (lambda* (A : Type) (ls : (List A)) Nat)
+          (lambda (A : Type) z)
+          (lambda* (A : Type) (a : A) (ls : (List A)) (ih : Nat)
+                   z)
+          Bool
+          (nil Bool))
+    Nat))
+
+
+(check-equal?
+ (void)
+ (:: list-ref (forall (A : Type) (->* (List A) Nat (Maybe A)))))
+(check-equal?
+ ((list-ref Bool (cons Bool true (nil Bool))) z)
+ (some Bool true))
