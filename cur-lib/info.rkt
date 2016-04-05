9958bf9440d3a80e6515414ec3356024a9ace3fe
William J. Bowman <wjb@williamjbowman.com>
2016-04-05 16:21:01 -0400

    Version 0.7 features: performance!
    Bumped version number; cur-test requires new performance fixes.

--- cur-lib/info.rkt
+++ cur-lib/info.rkt
@@ -3,5 +3,5 @@
 (define deps '("base" ("redex-lib" #:version "1.11")))
 (define build-deps '())
 (define pkg-desc "implementation (no documentation, tests) part of \"cur\".")
-(define version "0.6")
+(define version "0.7")
 (define pkg-authors '(wilbowma))
