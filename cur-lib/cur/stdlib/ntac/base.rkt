eb22e53d0f526c8a366f1251b606b2fc95033d39
William J. Bowman <wjb@williamjbowman.com>
2016-07-01 21:06:27 -0400

    Reorganized ntac, added syntax sugar
    * Renamed things to be a little more idiomatic.
    * Base structures and definitions provided by base.rkt
    * Moved proof tree commands and tacticals to standard.rkt
    * Developed a pattern for tacticals that results in better surface
      syntax, and helps hide namespace issue.
      TODO: Abstract pattern into macro.

---
+++ cur-lib/cur/stdlib/ntac/base.rkt
@@ -0,0 +1,192 @@
+#lang s-exp "../../main.rkt"
+;; Proof tree representation and top-level syntax
+
+(require
+ "../sugar.rkt")
+
+(provide
+ define-theorem
+ ntac-prove)
+
+(begin-for-syntax
+  (provide
+   ntac-syntax
+
+   qed
+   (struct-out ntt)
+   (struct-out ntt-hole)
+   make-ntt-hole
+   (struct-out ntt-exact)
+   make-ntt-exact
+   (struct-out ntt-context)
+   make-ntt-context
+   (struct-out ntt-apply)
+   make-ntt-apply
+   (struct-out ntt-done)
+   make-ntt-done
+
+   ;; proof tree zipper
+   (struct-out nttz)
+   make-nttz nttz-up nttz-down-context nttz-down-apply nttz-done?
+   next 
+
+   ;; In case someone wants to redefine define-theorem
+   new-proof-tree
+   proof-tree->complete-term 
+   eval-proof-script 
+   eval-proof-step
+   fntac-prove)
+
+  ;; NTac proof Tree
+  (struct ntt (contains-hole? goal) #:transparent)
+
+  ;; This is gross boilerplate to obtain default fields.
+  (struct ntt-hole ntt () #:transparent #:constructor-name _ntt-hole)
+  (define (make-ntt-hole goal)
+    (_ntt-hole #t goal))
+
+  (struct ntt-exact ntt (term) #:transparent #:constructor-name _ntt-exact)
+  (define (make-ntt-exact goal term)
+    (_ntt-exact #f goal term))
+
+  (struct ntt-context ntt (env-transformer parent) #:transparent #:constructor-name _ntt-context)
+  (define (make-ntt-context f k)
+    (_ntt-context (ntt-contains-hole? k) (ntt-goal k) f k))
+
+  (struct ntt-apply ntt (subterms tactic) #:transparent #:constructor-name _ntt-apply)
+  (define (make-ntt-apply goal subterms tactic)
+    (_ntt-apply (ormap ntt-contains-hole? subterms) goal subterms tactic))
+
+  (struct ntt-done ntt (parent) #:transparent #:constructor-name _ntt-done)
+  (define (make-ntt-done parent)
+    (when (ntt-contains-hole? parent)
+      (error 'ntt-done "Cannot construct done if hole present: ~v" parent))
+    (_ntt-done #f (ntt-goal parent) parent))
+
+  (define (new-proof-tree goal)
+    (make-ntt-hole goal))
+
+  (define (proof-tree->complete-term pt [err-stx #f])
+    (let loop ([pt pt])
+      (match pt
+        [(ntt-hole _ _)
+         (raise-syntax-error 'define-theorem "attempt to save incomplete proof" err-stx)]
+        [(ntt-exact _ _ a) a]
+        [(ntt-context _ _ gf k)
+         (loop k)]
+        [(ntt-apply _ _ cs f)
+         (apply f (map (λ (c) (loop c)) cs))]
+        [(ntt-done _ _ k)
+         (loop k)])))
+
+    ;; NTac proof Tree Zipper
+  (struct nttz (context focus parent-tree) #:constructor-name _nttz)
+
+  (define (make-nttz pt)
+    (_nttz (hasheq) pt
+         (λ (last-pt)
+           (make-nttz (make-ntt-done last-pt)))))
+
+  (define (nttz-up nttz)
+    ((nttz-parent-tree nttz) (nttz-focus nttz)))
+
+  (define (nttz-down-context tz)
+    (match-define (nttz context foc up) tz)
+    (match-define (ntt-context _ _ gf k) foc)
+    (_nttz (gf context) k (λ (new-k) (_nttz context (make-ntt-context gf new-k) up))))
+
+  (define (nttz-down-apply tz i)
+    (match-define (nttz context foc up) tz)
+    (match-define (ntt-apply _ a cs f) foc)
+    (define-values (before i+after) (split-at cs i))
+    (match-define (cons c_i after) i+after)
+    (_nttz context c_i
+         (λ (new-i) (_nttz context (make-ntt-apply a (append before (cons new-i after)) f) up))))
+
+  (define (nttz-done? tz)
+    (ntt-done? (nttz-focus tz)))
+
+  (define (fntac-prove ty ps)
+    (let ()
+      (define init-pt
+        (new-proof-tree (cur-expand ty)))
+      (define final-pt
+        (eval-proof-script
+         init-pt
+         (syntax->list ps)
+         ps))
+      (define pf
+        (proof-tree->complete-term
+         final-pt
+         ps))
+      pf))
+
+  (define (eval-proof-script pt psteps [err-stx #f])
+    (define last-nttz
+      (for/fold ([nttz (make-nttz pt)])
+                ([pstep-stx (in-list psteps)])
+        (eval-proof-step nttz pstep-stx)))
+    (qed last-nttz err-stx))
+
+  (define (eval-proof-step nttz pstep-stx)
+    ;; XXX Error handling on eval
+    ;; XXX Namespace is wrong
+    (define pstep (eval pstep-stx))
+    ;; XXX Error handling on what pstep is and what it returns
+    (pstep nttz))
+
+  (define (display-nttz tz)
+    (match (nttz-focus tz)
+      [(ntt-hole _ goal)
+       (for ([(k v) (in-hash (nttz-context tz))])
+         (printf "~a : ~a\n" k (syntax->datum v)))
+       (printf "--------------------------------\n")
+       (printf "~a\n" (syntax->datum goal))]
+      [(ntt-done _ _ _)
+       (printf "Proof complete.\n")]
+      [_
+       ;; XXX
+       (printf "Not at hole.\n")]))
+
+  (define (next tz)
+    (match (nttz-focus tz)
+      [(ntt-hole _ _) tz]
+      [(ntt-exact _ _ _) (next (nttz-up tz))]
+      [(ntt-context hole? _ _ k)
+       (next (if hole? (nttz-down-context tz) (nttz-up tz)))]
+      [(ntt-apply _ _ cs _)
+       (next
+        (or
+         (for/or ([i (in-naturals)]
+                  [c (in-list cs)])
+           (if (ntt-contains-hole? c)
+               (nttz-down-apply tz i)
+               #f))
+         (nttz-up tz)))]
+      [(ntt-done _ _ _)
+       tz]))
+
+  (define (qed nttz [err-stx #f])
+    (define up-nttz (next nttz))
+    (unless (nttz-done? up-nttz)
+      (raise-syntax-error 'qed "Proof incomplete.\n" err-stx))
+    (nttz-focus up-nttz))
+
+  (define anchor #'a)
+  (define (ntac-syntax syn)
+    (datum->syntax anchor (syntax->datum syn))))
+
+;; Syntax
+(define-syntax (define-theorem stx)
+  (syntax-parse stx
+    [(_ x:id ty . ps)
+     (quasisyntax/loc stx
+       (begin (define x (ntac-prove ty ps))
+              (:: x ty)))]))
+
+;; For inline ntac
+(define-syntax (ntac-prove stx)
+  (syntax-case stx ()
+    [(_ ty pf) (fntac-prove #'ty #'pf)])) 
+
+
---
+++ cur-lib/cur/stdlib/ntac/standard.rkt
@@ -0,0 +1,114 @@
+#lang s-exp "../../main.rkt"
+(require
+ "../sugar.rkt"
+ "base.rkt")
+
+;; define-nttz-cmd ?
+(define-for-syntax (nop ptz) ptz)
+
+(define-for-syntax (interactive ptz)
+  (define cmd-stx
+    (let/ec esc
+      (parameterize ([current-eval
+                      (λ (in)
+                        (syntax-case in ()
+                          [(_ . cmd)
+                           (esc (ntac-syntax #'cmd))]))])
+        (read-eval-print-loop))))
+  (define next-ptz
+    (eval-proof-step ptz cmd-stx))
+  (if (nttz-done? next-ptz)
+      next-ptz
+      (interactive next-ptz)))
+
+(define-for-syntax ((fill t) ptz)
+  (define new-foc (t (nttz-context ptz) (nttz-focus ptz)))
+  ;; XXX Maybe new-foc could be #f for failure?
+  (next
+   (struct-copy nttz ptz [focus new-foc])))
+
+;; define-tactical
+(define-for-syntax ((_intro [name #f]) ctxt pt)
+  (match-define (ntt-hole _ goal) pt)
+  (cur-match goal
+   [(forall (x:id : P:expr) body:expr)
+    (let ()
+      ;; NB: syntax is not hashable.
+      (define the-name (syntax->datum (or name #'x)))
+      (make-ntt-apply
+       goal
+       (list
+        (make-ntt-context
+         (λ (old-ctxt) (hash-set old-ctxt the-name #'P))
+         (make-ntt-hole #'body)))
+       (λ (body-pf)
+         #`(λ (#,the-name : P) #,body-pf))))]))
+
+;; A pattern emerges:
+;; tacticals must take additional arguments as ntac-syntax
+;; define-tactical should generate a phase 2 definition like the one below, and a functional version
+;; of the tactical (perhaps tactical-name-f)
+(begin-for-syntax
+  (define-syntax (intro syn)
+    (syntax-case syn ()
+      [(_)
+       #`(fill (_intro))]
+      [(_ syn)
+       #`(fill (_intro (ntac-syntax #'syn)))])))
+
+(define-for-syntax (_intros names)
+  (for/fold ([t nop])
+            ([n (in-list names)])
+    (compose (fill (_intro n)) t)))
+(begin-for-syntax
+  (define-syntax (intros syn)
+    (syntax-case syn ()
+      [(_ (id ...))
+       #`(_intros (list (ntac-syntax #'id) ...))])))
+
+;; define-tactical
+(define-for-syntax ((_exact a) ctxt pt)
+  (match-define (ntt-hole _ goal) pt)
+  (define env
+    (for/list ([(k v) (in-hash ctxt)])
+      (cons (datum->syntax #f k) v)))
+  (unless (cur-type-check? a goal #:local-env env)
+    (error 'exact "~v does not have type ~v" a goal))
+  (make-ntt-exact goal a))
+
+(begin-for-syntax
+  (define-syntax (exact syn)
+    (syntax-case syn ()
+      [(_ syn)
+       #`(fill (_exact (ntac-syntax #'syn)))])))
+
+;;define-tactical 
+(define-for-syntax (by-assumption ctxt pt)
+  (match-define (ntt-hole _ goal) pt)
+  (for/or ([(k v) (in-hash ctxt)]
+           ;; TODO: Probably need to add local-env to cur-equal?
+           #:when (cur-equal? v goal))
+    (make-ntt-exact goal k)))
+
+(module+ test
+  (require
+   "../prop.rkt"
+   "../sugar.rkt")
+
+  ;; Not quite and-proj1; need elim for that.
+  (define-theorem and-proj1
+    (forall (A : Type) (B : Type) (c : (And A B)) Type)
+    nop
+    ;; XXX The use of fill is ugly. Perhaps could be infered
+    (intro A)
+    (intro)
+    (intros (c))
+    nop
+    ;interactive ; try (fill (exact 'A))
+    ;; This works too
+    (exact A)
+    ;; And this
+    #;(fill by-assumption))
+
+  and-proj1
+  )
