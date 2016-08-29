1ad69784003be0a774e187bd045483ebbfa3ae83
William J. Bowman <wjb@williamjbowman.com>
2016-08-29 10:42:24 -0400

    Added slides describing TT and Racket macros

--- hope-2016/figures.rkt
+++ hope-2016/figures.rkt
@@ -8,3 +8,19 @@ define-language-sugar (let x = e₁ in e₂) ((λ (x : τ) e₂) e₁)
 
 define-theorem Type-Soundness
   if Γ ⊢ e : τ then e →* v
+
+
+(begin-for-syntax
+  ;; A function in the meta language
+  (define (transform-lambda obj)
+    (syntax-case obj (:)
+      [(_ (x : t) e)
+       #'(λ (x : t) e)])))
+
+;; A syntactic hook in the object language
+(define-syntax lambda transform-lambda)
+
+
+(lambda bla bla bla)
+;; causes the computation, at compile-time:
+(transform-lambda #'(lambda bla bla bla))
