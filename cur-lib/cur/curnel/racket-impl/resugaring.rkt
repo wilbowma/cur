9da3d175fdb80fb14ce9a07a56da906ca3d9b8f7
William J. Bowman <wjb@williamjbowman.com>
2016-11-12 03:09:00 -0500

    More experimenting with resugaring

--- cur-lib/cur/curnel/racket-impl/resugaring.rkt
+++ cur-lib/cur/curnel/racket-impl/resugaring.rkt
@@ -10,6 +10,8 @@
     [(syntax-property e 'origin)
      =>
      (lambda (x)
+     (displayln e)
+     (displayln x)
        (if (pair? x)
            (last x)
            x))]
@@ -17,15 +19,17 @@
 
 (define-for-syntax (resugar syn)
   (syntax-parse syn
-    #:datum-literals (#%app lambda quote)
+    #:datum-literals (#%app lambda quote #%expression)
     [x:id (origin-or syn)]
+    [(#%expression e)
+     (resugar #'e)]
     [(quote e)
      (origin-or #'e)]
     [(#%app f a)
-     #:when (not (syntax-property #'f 'constructor-for))
+     #:when (not (syntax-property syn 'origin))
      #`(#,(resugar #'f) #,(resugar #'a))]
     [(#%app f a)
-     #:when (syntax-property #'f 'constructor-for)
+;     #:when (syntax-property #'f 'constructor-for)
      #`(#,(origin-or syn) #,(resugar #'a))]
     [(lambda (x) body)
      #`(#,(origin-or syn) (#,(resugar #'x))
@@ -36,8 +40,8 @@
   (syntax-case syn ()
     [(_ e)
      (begin 
-       (printf "Expanded ~a~n" (local-expand #'e 'expression null))
-       (printf "Resugared ~a~n" (resugar (local-expand #'e 'expression null)))
+       (printf "Expanded ~a~n" (syntax->datum (local-expand #'e 'expression null)))
+       (printf "Resugared ~a~n" (syntax->datum (resugar (local-expand #'e 'expression null))))
        #'(void))]))
 
 (struct meow (e))
@@ -47,3 +51,27 @@
 (origin (meow 1))
 
 (origin (λ (x) x))
+
+(define-syntax (λ* syn)
+  (syntax-case syn ()
+    [(_ () body)
+     #`body]
+    [(_ (x xs ...) body)
+     #`(lambda (x) (λ* (xs ...) body))]))
+
+(define-syntax (app* syn)
+  (syntax-case syn ()
+    [(_ e)
+     #`e]
+    [(_ e1 e2 es ...)
+     #`(app* (e1 e2) es ...)]))
+
+(define-syntax (my-let syn)
+  (syntax-case syn ()
+    [(_ ([x e] ...) body)
+     #`(app* (λ* (x ...) body) e ...)]))
+
+;; Okay so, resugaring could work over the curnel, but not for arbitrary macros... but even then, I'm
+;; skeptical. Might end up with very bad error messages, but at the very least could use to blame the
+;; right macro.
+(origin (my-let ([x 5] [y 6]) y))
