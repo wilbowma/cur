3efbdc40e4cbc302c91f40e108269bf65904384c
William J. Bowman <wjb@williamjbowman.com>
2016-11-18 23:58:16 -0500

    Benchmarked: old: 9m 50s; new: 27s

--- cur-lib/cur/main.rkt
+++ cur-lib/cur/main.rkt
@@ -1,5 +1,5 @@
 #lang racket/base
 
-;; The default language is "fast-model"
+;; The default language is the Types as Macro/Racket implementation
 (require "curnel/racket-impl/lang.rkt")
 (provide (all-from-out "curnel/racket-impl/lang.rkt"))
