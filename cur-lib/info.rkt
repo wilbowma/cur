bee6d2af0b85a7df5a49f505fe7c57a233a005b9
William J. Bowman <wjb@williamjbowman.com>
2016-11-22 00:47:56 -0500

    Trying to remove redex dependency; but not working

--- cur-lib/info.rkt
+++ cur-lib/info.rkt
@@ -1,7 +1,8 @@
 #lang info
 (define collection 'multi)
-(define deps '("base" ("redex-lib" #:version "1.11")))
+(define deps '("base"))
 (define build-deps '())
 (define pkg-desc "implementation (no documentation, tests) part of \"cur\".")
-(define version "0.12")
+(define compile-omit-paths '("cur/curnel/snoc-env.rkt" "cur/curnel/model" "cur/curnel/racket-impl/resugar.rkt"))
+(define version "0.2")
 (define pkg-authors '(wilbowma))
