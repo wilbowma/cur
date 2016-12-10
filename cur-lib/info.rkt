6f8793a68655739fa47b3a8dca064b001dc10c34
William J. Bowman <wjb@williamjbowman.com>
2016-12-10 17:25:53 -0500

    Trying to avoid compilation w/o disabling setup

--- cur-lib/info.rkt
+++ cur-lib/info.rkt
@@ -5,5 +5,4 @@
 (define pkg-desc "implementation (no documentation, tests) part of \"cur\".")
 (define compile-omit-paths 'all #;'("cur/curnel/snoc-env.rkt" "cur/curnel/model" "cur/curnel/racket-impl/resugaring.rkt"))
 (define version "0.2")
-(define setup-collects '())
 (define pkg-authors '(wilbowma))
