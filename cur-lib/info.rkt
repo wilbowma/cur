5c72773977dbea15fd4d3f7a7473d713751ac2a0
William J. Bowman <wjb@williamjbowman.com>
2016-12-10 16:51:12 -0500

    Try to force raco to not compile things

--- cur-lib/info.rkt
+++ cur-lib/info.rkt
@@ -5,4 +5,5 @@
 (define pkg-desc "implementation (no documentation, tests) part of \"cur\".")
 (define compile-omit-paths 'all #;'("cur/curnel/snoc-env.rkt" "cur/curnel/model" "cur/curnel/racket-impl/resugaring.rkt"))
 (define version "0.2")
+(define setup-collects '())
 (define pkg-authors '(wilbowma))
