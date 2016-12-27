2277eb8b3634f6e2427060b0e8004b9ecf626b6f
William J. Bowman <wjb@williamjbowman.com>
2016-12-26 20:24:57 -0500

    Updating info files

--- cur-lib/cur/info.rkt
+++ cur-lib/cur/info.rkt
@@ -1,3 +1,3 @@
 #lang info
 (define collection 'multi)
-(define compile-omit-paths 'all #;'("cur/curnel/snoc-env.rkt" "cur/curnel/model" "cur/curnel/racket-impl/resugaring.rkt"))
+(define compile-omit-paths 'all)
--- cur-lib/info.rkt
+++ cur-lib/info.rkt
@@ -3,6 +3,5 @@
 (define deps '("base"))
 (define build-deps '())
 (define pkg-desc "implementation (no documentation, tests) part of \"cur\".")
-(define compile-omit-paths 'all #;'("cur/curnel/snoc-env.rkt" "cur/curnel/model" "cur/curnel/racket-impl/resugaring.rkt"))
 (define version "0.2")
 (define pkg-authors '(wilbowma))
