219943bcc66f906143f6c7405bd05e85b78c4918
William J. Bowman <wjb@williamjbowman.com>
2016-12-27 21:26:12 -0500

    Removed cur-model from collection for now

--- cur/info.rkt
+++ cur/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection 'multi)
-(define deps '("cur-lib" "cur-model" "cur-doc" "cur-test"))
-(define implies '("cur-lib" "cur-model" "cur-doc" "cur-test"))
+(define deps '("cur-lib" "cur-doc" "cur-test"))
+(define implies '("cur-lib" "cur-doc" "cur-test"))
 (define pkg-desc "Dependent types with parenthesis and meta-programming.")
 (define pkg-authors '(wilbowma))
