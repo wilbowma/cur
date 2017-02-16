02b18adc30e7da313738fd83a99186799ebfa20e
William J. Bowman <wjb@williamjbowman.com>
2017-02-16 18:20:16 -0500

    Added some utilities for writing syntax classes

---
+++ cur-lib/cur/curnel/racket-impl/stxutils.rkt
@@ -0,0 +1,29 @@
+#lang racket/base
+(require
+ (for-syntax
+  racket/base
+  syntax/parse
+  racket/syntax)
+ syntax/parse
+ syntax/parse/experimental/reflect)
+
+(provide (all-defined-out))
+
+(define (local-expand-expr x) (local-expand x 'expression null))
+
+(define (reified-syntax-class->pred stxclass)
+  (lambda (expr)
+    (syntax-parse (local-expand-expr expr)
+      [(~reflect _ (stxclass)) #t]
+      [_ #f])))
+
+(define-syntax-rule (syntax-class->pred id)
+  (reified-syntax-class->pred (reify-syntax-class id)))
+
+(define-syntax (define-syntax-class/pred stx)
+  (syntax-parse stx
+    [(_ name:id expr ...)
+     #:with pred? (format-id #'name "~a?" #'name)
+     #`(begin
+         (define-syntax-class name expr ...)
+         (define pred? (syntax-class->pred name)))]))
