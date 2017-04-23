080483235fbb66067c458e3a0498d03a112c732a
William J. Bowman <wjb@williamjbowman.com>
2017-04-23 02:35:24 -0400

    Debugged typing of snd

--- cur-lib/cur/stdlib/sigma.rkt
+++ cur-lib/cur/stdlib/sigma.rkt
@@ -28,7 +28,7 @@
             (λ (a : A) (b : (P a)) a)))
 
 (define (snd0 (A : (Type 0)) (P : (-> A (Type 0))) (p : (Σ0 A P)))
-  (new-elim p (λ (x : (Σ0 A P)) (P (run (fst0 A P x))))
+  (new-elim p (λ (x : (Σ0 A P)) (P (fst0 A P x)))
             (λ (a : A) (b : (P a)) b)))
 
 (define (fst1 (A : (Type 0)) (P : (-> A (Type 0))) (p : (Σ1 A P)))
@@ -36,7 +36,7 @@
             (λ (a : A) (b : (P a)) a)))
 
 (define (snd1 (A : (Type 0)) (P : (-> A (Type 0))) (p : (Σ1 A P)))
-  (new-elim p (λ (x : (Σ1 A P)) (P (run (fst1 A P x))))
+  (new-elim p (λ (x : (Σ1 A P)) (P (fst1 A P x)))
             (λ (a : A) (b : (P a)) b)))
 
 #;(define (fst0 (A : (Type 1)) (P : (-> A (Type 1))) (p : (Σ0 A P)))
