c6a9f9f3824674a54d1adb20bca66b02a8938852
William J. Bowman <wjb@williamjbowman.com>
2017-08-15 15:28:43 -0400

    Added test for issue 67; combined with prior commit, closes #67

---
+++ cur-test/cur/tests/issue67.rkt
@@ -0,0 +1,22 @@
+#lang cur
+
+(data Nat : 0 (Type 0)
+      (z : Nat)
+      (s : (Π (x : Nat) Nat)))
+
+(data Nat2 : 0 (Type 0)
+      (zz : Nat2)
+      (ss : (Π (x : Nat2) Nat2)))
+
+(begin-for-syntax
+  (require chk racket/base)
+  (chk
+   #:x
+   (local-expand #'(new-elim z
+                             (λ (x : Nat) Nat)
+                             zz
+                             (λ (x : Nat2) (λ (ih : Nat2) ih)))
+                 'expression
+                 '())
+   "Expected type Nat, but found type of Nat2 while checking method for z"))
+
