efa43ca7e743252ac9a6954606ab0d0d08c3cf26
William J. Bowman <wjb@williamjbowman.com>
2017-08-24 13:48:08 -0400

    Switch default core to turnstile

--- cur-lib/cur/main.rkt
+++ cur-lib/cur/main.rkt
@@ -1,5 +1,5 @@
 #lang racket/base
 
 ;; The default language is the Types as Macro/Racket implementation
-(require "curnel/racket-impl/lang.rkt")
-(provide (all-from-out "curnel/racket-impl/lang.rkt"))
+(require "curnel/turnstile/dep-ind.rkt")
+(provide (all-from-out "curnel/turnstile/dep-ind.rkt"))
