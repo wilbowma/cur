3739f87aaa35c2c3cd3c32ae5e5aeaf8ef1276bd
William J. Bowman <wjb@williamjbowman.com>
2017-09-14 10:49:07 -0400

    Added dependency on turnstile

--- cur-lib/info.rkt
+++ cur-lib/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection 'multi)
-(define deps '("base"))
+(define deps '("base" "https://bitbucket.org/stchang/macrotypes#peano"))
 (define build-deps '())
 (define pkg-desc "implementation (no documentation, tests) part of \"cur\".")
 (define version "0.35")
