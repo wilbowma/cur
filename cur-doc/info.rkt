c3e03ec6a017fcbaed9dcf1917e014f3cec345b9
William J. Bowman <wjb@williamjbowman.com>
2017-11-15 15:42:23 +0100

    Updated dependencies for cur-doc

--- cur-doc/info.rkt
+++ cur-doc/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection 'multi)
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "sandbox-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "sandbox-lib" "cur-lib"))
 (define pkg-desc "Documentation for \"cur\".")
 (define pkg-authors '(wilbowma))
