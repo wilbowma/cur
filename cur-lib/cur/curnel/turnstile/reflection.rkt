647eb6bb4aec7c5fe2f576537c2a2c47e1f8dbb2
Marcela Poffald <mpoffald@ambroses-mbp.attlocal.net>
2018-02-08 20:56:16 -0500

    stub out cur-expand, notes on lambda failures

--- cur-lib/cur/curnel/turnstile/reflection.rkt
+++ cur-lib/cur/curnel/turnstile/reflection.rkt
@@ -12,7 +12,7 @@
 ; (for-template "type-check.rkt")
  ;(for-template "runtime.rkt")
  (for-template (only-in turnstile/lang infer typecheck? current-type-eval))
- (for-template  (rename-in turnstile/examples/dep-ind-cur [#%app dep-#%app]))
+ (for-template  turnstile/examples/dep-ind-cur )
  (for-template "cur-to-turnstile.rkt")
  (for-template (only-in racket/base quote ))
  )
@@ -48,16 +48,16 @@
    ; (displayln (format "(inferred: ~a \n expected: ~a" inferred-type expected-type))
     (typecheck? inferred-type expected-type)))
 
-(define (cur->datum syn) 
-  (let ([expanded (local-expand syn 'expression null)])
+(define (cur->datum syn) ;write special case for each core turn- form instead? 
+  (let ([expanded (cur-expand syn)])
     ;(displayln (format "expanded: ~a" expanded))
     (let ([reflected (cur-reflect expanded)])
      ; (displayln (format "reflected: ~a" reflected))
       (syntax->datum reflected))))
 
 (define (cur-normalize syn)
-  (let ([expanded (local-expand syn 'expression null)])
-    (cur-reflect ((current-type-eval) expanded))))
+  (let ([evaled ((current-type-eval) syn)])
+    (cur-reflect evaled)))
 
 (define (cur-reflect syn) 
   (syntax-parse syn #:literals ( quote λ Type)
@@ -67,7 +67,7 @@
      syn]
     [(_ _ (_ () _ (_ _ (quote i:exact-nonnegative-integer))))
      #'(Type i)]
-    [(λ (x : type) body)
+    [(λ (x : type) body)  ;dep-ind-cur throws away the arg type, never reaches this case in cur-normalize or cur->datum
      #`(λ (#,(cur-reflect #'x) : #,(cur-reflect #'type)) #,(cur-reflect #'body))]
     [(_  _ (_ (x)
              _ (_  _  (_ _
@@ -76,3 +76,6 @@
                   (_ _  arg-type body-type))))))
      #`(Π (x : #,(cur-reflect #'arg-type)) #,(cur-reflect #'body-type))]
     ))
+
+(define (cur-expand syn) 
+  (local-expand syn 'expression null))
