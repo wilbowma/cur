fd46d9a6ca12af4e24e8e5c365f75f7087a75d97
Marcela Poffald <mpoffald@ambroses-mbp.attlocal.net>
2018-02-08 20:54:37 -0500

    comments on failures for future reference

--- cur-test/cur/tests/curnel/reflection.rkt
+++ cur-test/cur/tests/curnel/reflection.rkt
@@ -28,17 +28,17 @@
    #:eq cur-equal? (cur-type-infer #'(turn-app (turn-λ (x : (turn-Type 1)) x) (turn-Type 0))) #'(Type 1)
    )
  (chk
-   ;#:t (cur-type-check? #'(turn-λ (x : (turn-Type 0)) x)  #'(Π (x : (Type 0)) (Type 0))) ;fails typecheck?
+   #:t (cur-type-check? #'(turn-λ (x : (turn-Type 0)) x)  #'(Π (x : (Type 0)) (Type 0))) ;expected and inferred are (Π (x : (Type 0)) (Type 0)), but typecheck? returns #f
    #:t (cur-type-check? #'(turn-Type 0) #'(Type 1))
    )
  (chk
-  ;#:= (cur->datum #'a) 'a
+  
   #:= (cur->datum #'(turn-Type 0)) '(Type 0)
- ; #:= (cur->datum #'(turn-λ (a : (turn-Type 0)) a)) '(λ (a : (Type 0)) a) ;lambda: bad syntax in (lambda (a) a)
+  #:= (cur->datum #'(turn-λ (a : (turn-Type 0)) a)) '(λ (a : (Type 0)) a) ;fails at dep-ind-cur: bad syntax in (lambda (a) a)
   #:= (cur->datum #'(turn-Π (a : (turn-Type 0)) (turn-Type 0))) '(Π (a : (Type 0)) (Type 0))
   )
  (chk
   #:eq cur-equal? (cur-normalize #'(turn-Type 1)) #'(Type 1)
- ; #:eq cur-equal? (cur-normalize #'(turn-λ (x : (turn-Type 0)) x)) #'(λ (x : (Type 0)) x) ;fails: bad syntax in (lambda (x) x)
+  #:eq cur-equal? (cur-normalize #'(turn-λ (x : (turn-Type 0)) x)) #'(λ (x : (Type 0)) x) ;fails at dep-ind-cur: bad syntax in (lambda (x) x)
   #:eq cur-equal? (cur-normalize #'(turn-app (turn-λ (x : (turn-Type 1)) x) (turn-Type 0))) #'(Type 0)
   #:eq cur-equal? (cur-normalize #'(((turn-λ (A : (turn-Type 3)) (turn-λ (a : (turn-Type 2)) a)) (turn-Type 2)) (turn-Type 1))) #'(Type 1) ))
