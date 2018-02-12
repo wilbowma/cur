0b1ae7ffa61673d73bbee5062114a14ad7959d52
William J. Bowman <wjb@williamjbowman.com>
2018-02-12 16:31:26 -0500

    Updated dependency

--- cur-lib/info.rkt
+++ cur-lib/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection 'multi)
-(define deps '("base" "https://bitbucket.org/stchang/macrotypes#peano"))
+(define deps '("base" "https://github.com/stchang/macrotypes.git#dep"))
 (define build-deps '())
 (define pkg-desc "implementation (no documentation, tests) part of \"cur\".")
 (define version "0.35")
