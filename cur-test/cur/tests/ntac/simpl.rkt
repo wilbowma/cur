21a6b5c04760b39e9489d3fa8961268286297522
Stephen Chang <stchang@ccs.neu.edu>
2018-05-08 13:37:08 -0400

    Added basic ntac tactics and tests from Software Foundations
    - Implemented basic tactics including
      - destruct
      - induction
      - simpl
    - Implemented equality tactics, for both Martin-Löf (ML-=) equality and
      Paulin-Mohring equality (PM-= aka ==)
      - rewrite, and friends
      - reflexivity
    - Added tests based on working through Software Foundations Ch. 1--3, using
      PM equality (per Software Foundations), and testing ML equality when
      convenient.

---
+++ cur-test/cur/tests/ntac/simpl.rkt
@@ -0,0 +1,58 @@
+#lang cur
+
+(require
+ rackunit
+ cur/stdlib/nat
+ cur/stdlib/bool
+ (rename-in (except-in cur/stdlib/equality == refl)
+            [ML-= ==]
+            [ML-refl refl])
+ cur/stdlib/sugar
+ cur/ntac/base
+ cur/ntac/standard
+ cur/ntac/ML-rewrite)
+
+;; tests for simpl tactic
+;; (but most examples from SF Ch 1: Basics)
+
+(data day : 0 (Type 0)
+      (mon : day)
+      (tues : day)
+      (wed : day)
+      (thurs : day)
+      (fri : day)
+      (sat : day)
+      (sun : day))
+
+(define next-weekday
+  (λ [d : day]
+    (new-elim d
+              (λ [x : day] day)
+              tues
+              wed
+              thurs
+              fri
+              mon
+              mon
+              mon)))
+
+(check-equal? (next-weekday fri) mon)
+(check-equal? (next-weekday (next-weekday sat)) tues)
+
+(:: (refl day tues)
+    (== day (next-weekday (next-weekday sat)) tues))
+
+(:: (refl day (next-weekday (next-weekday sat)))
+    (== day (next-weekday (next-weekday sat)) tues))
+
+(ntac (== day (next-weekday (next-weekday sat)) tues)
+      simpl
+      reflexivity)
+
+
+;; tests that simpl uses ctx
+(define-theorem plus_1_l
+  (∀ [n : Nat] (== Nat (plus 1 n) (s n)))
+  by-intro
+  simpl
+  reflexivity)
