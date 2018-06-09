abdc9edbe9bef3d0f6b736507f1ae7c21823ba8a
Marcela (racket) <mpoffald@gmail.com>
2018-06-09 17:44:39 -0400

    add datatype testing

--- cur-test/cur/tests/curnel/local-env.rkt
+++ cur-test/cur/tests/curnel/local-env.rkt
@@ -31,16 +31,25 @@
  "../../../../cur-lib/cur/curnel/turnstile/stxutils.rkt"
  "../../../../cur-lib/cur/curnel/turnstile/reflection.rkt"))
 
+(data Nat : 0 (Type 0)
+           (z :   Nat)
+           (s : (Π (x : Nat) Nat)))
+
 (begin-for-syntax
   (define test-env1 `((,#'x . ,#'X) (,#'X . ,#'(Type 1))))
+  (define test-env2 `((,#'f . ,#'(Π (x : (Type 0)) (Type 0))) (,#'x . ,#'(Type 0))))
+  (define test-env3 `((,#'x . ,#'Nat)))
+  (displayln (cur-normalize #'X #:local-env test-env1))
+  (displayln (cur-type-infer #'x #:local-env test-env1))
   (chk
    #:x (cur-equal? #'x #'x) ""
    #:t (cur-equal? #'x #'x #:local-env `((,#'x . ,#'(Type 0))))
    #:t (cur-type-infer #'X #:local-env `((,#'X . ,#'(Type 0))))
    #:t (cur-type-infer #'X #:local-env test-env1)
+   #:t (cur-type-infer #'x #:local-env test-env3)
+   #:t (cur-normalize #'(f x) #:local-env test-env2)
    #:eq cur-equal? (cur-type-infer #'X #:local-env `((,#'X . ,#'(Type 0)))) #'(Type 0)
    #:eq cur-equal? (cur-type-infer #'X #:local-env test-env1) #'(Type 1)
-
    ;; When comparing open terms, need to specify the environment under which they are equal
    #:eq (lambda (x y) (cur-equal? x y #:local-env test-env1))
    (cur-type-infer #'x #:local-env test-env1)
@@ -48,7 +57,6 @@
 
    #:t (cur-type-check? #'x #'(Type 0) #:local-env `((,#'x . ,#'(Type 0))))
 
-   #:do (define test-env2 `((,#'f . ,#'(Π (x : (Type 0)) (Type 0))) (,#'x . ,#'(Type 0))))
    #:eq cur-equal? (cur-type-infer #'f #:local-env test-env2)
    #'(Π (x : (Type 0)) (Type 0))
 
