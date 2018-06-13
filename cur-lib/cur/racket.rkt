8d378df89cd84b59f9f26eea785f5866592a6828
William J. Bowman <wjb@williamjbowman.com>
2018-06-13 13:22:53 -0400

    Added cur/racket and cur/turnstile langs

---
+++ cur-lib/cur/racket.rkt
@@ -0,0 +1,5 @@
+#lang racket/base
+
+; The default language is the Types as Macro/Racket implementation
+(require "curnel/racket-impl/lang.rkt")
+(provide (all-from-out "curnel/racket-impl/lang.rkt"))
