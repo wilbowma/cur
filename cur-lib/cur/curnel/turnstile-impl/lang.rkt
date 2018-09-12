52b83495460d5e48e3f2733eefb4ad6ee622ddb5
Stephen Chang <stchang@ccs.neu.edu>
2018-09-12 14:35:25 -0400

    start turnstile reflection lib; datum tests passing

--- cur-lib/cur/curnel/turnstile-impl/lang.rkt
+++ cur-lib/cur/curnel/turnstile-impl/lang.rkt
@@ -3,7 +3,8 @@
 (provide ;; #%module-begin
          provide require only-in for-syntax all-from-out
          ;; begin-for-syntax
-         ;; define-values begin define-syntax define #%app λ
+         ;; define-values begin define #%app λ
+         define-syntax define-for-syntax
          (for-syntax
           (all-from-out racket/base
                         syntax/parse
@@ -19,6 +20,9 @@
 
 (require "dep-ind-cur2+data2.rkt")
 (provide (all-from-out "dep-ind-cur2+data2.rkt"))
+
+(require "reflection.rkt")
+(provide (all-from-out "reflection.rkt"))
 
 ;; (define-syntax define+provide-placeholders
 ;;   (syntax-parser
