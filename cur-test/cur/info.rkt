57a0d87688c54e3dab99ab6969072df8a90f3ea0
Stephen Chang <stchang@ccs.neu.edu>
2018-09-17 16:26:30 -0400

    cur-test: tweak test-omit-paths in info.rkt

--- cur-test/cur/info.rkt
+++ cur-test/cur/info.rkt
@@ -1,3 +1,18 @@
 #lang info
 (define collection 'multi)
 (define compile-omit-paths 'all)
+(define test-omit-paths
+  '("tests/curnel/"
+    "tests/issue-71.rkt"
+    "tests/ntac/"
+    "tests/ntac.rkt"
+    "tests/olly.rkt"
+    "tests/stdlib/ascii.rkt"
+    "tests/stdlib/list.rkt"
+    "tests/stdlib/maybe.rkt"
+    "tests/stdlib/racket-ascii.rkt"
+    "tests/stdlib/sigma.rkt"
+    "tests/stdlib/typeclass.rkt"
+    "tests/stlc.rkt"
+    "tests/sweet-exp.rkt"
+    "tests/vector-append.rkt"))
--- cur-test/info.rkt
+++ cur-test/info.rkt
@@ -5,18 +5,4 @@
 (define update-implies '("cur-lib"))
 (define pkg-desc "Tests for \"cur\".")
 (define pkg-authors '(wilbowma))
-(define test-omit-paths
-  '("cur/tests/curnel/"
-    "cur/tests/issue-71.rkt"
-    "cur/tests/ntac/"
-    "cur/tests/ntac.rkt"
-    "cur/tests/olly.rkt"
-    "cur/tests/stdlib/ascii.rkt"
-    "cur/tests/stdlib/list.rkt"
-    "cur/tests/stdlib/maybe.rkt"
-    "cur/tests/stdlib/racket-ascii.rkt"
-    "cur/tests/stdlib/sigma.rkt"
-    "cur/tests/stdlib/typeclass.rkt"
-    "cur/tests/stlc.rkt"
-    "cur/tests/sweet-exp.rkt"
-    "cur/tests/vector-append.rkt"))
+
