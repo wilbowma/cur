017d1e68a43d96580647ff2e6b4c006ddc11bb03
Stephen Chang <stchang@ccs.neu.edu>
2018-09-17 16:24:02 -0400

    fix data shim (and define-datatype) to handle simple case; add "plus" test

--- cur-test/cur/tests/plus.rkt
+++ cur-test/cur/tests/plus.rkt
@@ -2,23 +2,19 @@
 
 (require
  cur/stdlib/sugar
- rackunit)
+ turnstile/rackunit-typechecking)
 
 (data Nat : 0 Type
   (z : Nat)
   (s : (Π (x : Nat) Nat)))
 
-(plus . : . (-> Nat Nat Nat))
-(define (plus n m)
-  (match n
-    [z m]
-    [(s (x : Nat))
-     (s (recur x))]))
+;; ;(plus . : . (-> Nat Nat Nat))
+;; (define (plus [n : Nat] [m : Nat] -> Nat)
+;;   (match n
+;;     [z m]
+;;     [(s (x : Nat))
+;;      (s (recur x))]))
 
-(check-equal?
- (plus z z)
- z)
+;; (check-type (plus z z) : Nat -> z)
 
-(check-equal?
- (plus (s z) z)
- (s z))
+;; (check-type (plus (s z) z) : Nat -> (s z))
