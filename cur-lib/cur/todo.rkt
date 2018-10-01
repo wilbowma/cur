4d6ba3d168c4200d091f1cfbf68e7be1608ff073
William J. Bowman <wjb@williamjbowman.com>
2018-09-30 19:36:18 -0500

    Fixed some issues with holes
    - Fixed lost source location, which prevent working across modules
    - Minor fixes

--- cur-lib/cur/todo.rkt
+++ cur-lib/cur/todo.rkt
@@ -2,38 +2,35 @@
 
 (require
  "curnel/racket-impl/runtime.rkt"
+ "curnel/racket-impl/reflection.rkt"
  (for-syntax todo-list "curnel/racket-impl/runtime-utils.rkt"))
 
 (provide TODO)
 
 (data False : 0 (Type 0))
-(axiom BADNESS10000 : False)
+(axiom HOLE : False)
 
 (define-for-syntax (pretty-print-env g)
   (for/fold
       ([str ""])
       ([(k v) (in-dict g)])
-    (format "~a~a : ~a~n" str (syntax-e k) (syntax-e (identifier-info-type v)))))
+    (format "~a~a : ~a~n" str (syntax-e k) (cur->datum (identifier-info-type v)))))
 
 (define-for-syntax (hline str)
   (build-string (max (sub1 (string-length str)) 0) (lambda _ #\-)))
 
 (define-syntax (TODO stx)
   (syntax-parse stx
-    [(_ type) #`(TODO type "TODO: Something of type ~a.")]
+    [(_ type)
+     (syntax/loc stx
+       (TODO type "TODO: Something of type ~a."))]
     [(_ type msg)
-     (let* ([env (pretty-print-env (gamma))]
-            [item (todo-item (format "~a~a~n~a"
-                                     env
-                                     (hline env)
-                                     (syntax-e #'type))
-                             (format (syntax-e #'msg)
-                                     (syntax-e #'type)))])
+     (let* ([t (syntax-e #'type)]
+            [env (pretty-print-env (gamma))]
+            [item (todo-item (format "~a~a~n~a" env (hline env) t)
+                             (format (syntax-e #'msg) t))])
        (syntax-property
-        #`(new-elim BADNESS10000 (λ (x : False) type))
+        (syntax/loc stx (new-elim HOLE (λ (x : False) type)))
         'todo
         item
         #t))]))
-
-(require "stdlib/nat.rkt")
-(TODO Nat)
