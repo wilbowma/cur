6c1fb3762841f89bc47585031f10032fc126e8e4
Stephen Chang <stchang@ccs.neu.edu>
2018-10-12 16:34:30 -0400

    port stdlib/list and enable list tests

--- cur-test/cur/tests/stdlib/list.rkt
+++ cur-test/cur/tests/stdlib/list.rkt
@@ -1,76 +1,102 @@
 #lang cur
 (require
- rackunit
+ turnstile/rackunit-typechecking
  cur/stdlib/sugar
  cur/stdlib/bool
  cur/stdlib/nat
  cur/stdlib/maybe
  cur/stdlib/list)
 
-(check-equal?
- nil
- nil)
-;; NB HACK: Hack to register :: as a test-case.
-;; TODO: Abstract this away
-(check-equal?
- (void)
- (:: (cons Bool true (nil Bool)) (List Bool)))
+(check-type (nil Nat) : (List Nat) -> (nil Nat))
+(check-not-type (nil Nat) : (List Bool))
 
-(check-equal?
- (nil Nat)
- (build-list Nat))
+(check-type (cons Bool true (nil Bool)) : (List Bool))
 
-(check-equal?
- (cons Nat z (nil Nat))
- (build-list Nat z))
+(check-type (build-list Nat) : (List Nat) -> (nil Nat))
+(check-type (build-list Nat z) : (List Nat) -> (cons Nat z (nil Nat)))
+(check-type (build-list Nat z z) : (List Nat) -> (cons Nat z (cons Nat z (nil Nat))))
 
-(check-equal?
- (cons Nat z (cons Nat z (nil Nat)))
- (build-list Nat z z))
+(check-type
+ (lambda (A : Type) (a : A)
+         (ih-a : (-> Nat (Maybe A)))
+         (n : Nat)
+         (match n #:return (Maybe A)
+           [z (some A a)]
+           [(s (n-1 : Nat))
+            (ih-a n-1)]))
+ : (Π (A : Type) (a : A) (ih-a : (-> Nat (Maybe A)))
+      (n : Nat)
+      (Maybe A)))
+(check-type
+ (lambda (A : Type) (n : Nat) (none A))
+ : (Π (A : Type) (-> Nat (Maybe A))))
+(check-type
+ (elim-List (nil Bool)
+            (lambda (ls : (List Bool)) Nat)
+            z
+            (lambda (a : Bool) (ls : (List Bool)) (ih : Nat)
+                    z))
+ : Nat)
 
-(check-equal?
- (void)
- (:: (lambda (A : Type) (a : A)
-             (ih-a : (-> Nat (Maybe A)))
-             (n : Nat)
-             (match n
-               [z (some A a)]
-               [(s (n-1 : Nat))
-                (ih-a n-1)]))
-     (forall (A : Type) (a : A) (ih-a : (-> Nat (Maybe A)))
-             (n : Nat)
-             (Maybe A))))
-(check-equal?
- (void)
- (:: (lambda (A : Type) (n : Nat) (none A)) (forall (A : Type) (-> Nat (Maybe A)))))
-(check-equal?
- (void)
- (:: (elim List (lambda (ls : (List Bool)) Nat)
-          (z
-           (lambda (a : Bool) (ls : (List Bool)) (ih : Nat)
-                   z))
-          (nil Bool))
-    Nat))
+;; list-ref
+(check-type (list-ref Bool (nil Bool) z)
+            : (Maybe Bool)
+            -> (none Bool))
+(check-type (list-ref Bool (nil Bool) (s z))
+            : (Maybe Bool)
+            -> (none Bool))
+(check-type (list-ref Bool (nil Bool) (s (s z)))
+            : (Maybe Bool)
+            -> (none Bool))
+(check-type (list-ref Bool (cons Bool true (nil Bool)) z)
+            : (Maybe Bool)
+            -> (some Bool true))
+(check-type (list-ref Bool (cons Bool false (cons Bool true (nil Bool))) z)
+            : (Maybe Bool)
+            -> (some Bool false))
+(check-type (list-ref Bool (cons Bool false (cons Bool true (nil Bool))) (s z))
+            : (Maybe Bool)
+            -> (some Bool true))
+(check-type (list-ref Bool (cons Bool false (cons Bool true (nil Bool))) (s (s z)))
+            : (Maybe Bool)
+            -> (none Bool))
 
+;; list-ref: non-full applications
+(check-type list-ref : (Π [A : Type] (-> (List A) Nat (Maybe A))))
+(check-type (list-ref Bool) : (-> (List Bool) Nat (Maybe Bool)))
+(check-type (list-ref Nat) : (-> (List Nat) Nat (Maybe Nat)))
+(check-type (list-ref (List Nat)) : (-> (List (List Nat)) Nat (Maybe (List Nat))))
+(check-type (list-ref Bool (nil Bool)) : (-> Nat (Maybe Bool)))
+;; TODO: fix this err msg
+(typecheck-fail (list-ref Bool (nil Nat))
+;                #:with-msg "expected (List Bool), given (List Nat)")
+                #:with-msg "expected \\(List A\\), given \\(List A\\)")
+(check-type ((list-ref Bool (nil Bool)) z) : (Maybe Bool) -> (none Bool))
 
-(check-equal?
- (void)
- (:: list-ref (forall (A : Type) (-> (List A) Nat (Maybe A)))))
-(check-equal?
- ((list-ref Bool (cons Bool true (nil Bool))) z)
- (some Bool true))
+(check-type (length Nat (nil Nat)) : Nat -> 0)
+(check-type (length Bool (nil Bool)) : Nat -> 0)
+;; TODO: fix this err msg
+(typecheck-fail (length Bool (nil Nat))
+;                #:with-msg "expected (List Bool), given (List Nat)")
+                #:with-msg "expected \\(List A\\), given \\(List A\\)")
+(check-type (length Nat (cons Nat z (nil Nat))) : Nat -> 1)
 
-;; TODO: Produces bad error message
-; (((list-ref Bool (cons Bool true (nil Bool))) z) z)
+(check-type
+ (list-append Nat (nil Nat) (nil Nat))
+ : (List Nat)
+ -> (nil Nat))
 
-(check-equal?
- (nil Nat)
- (list-append Nat (nil Nat) (nil Nat)))
+(check-type
+ (list-append Nat (nil Nat) (cons Nat z (nil Nat)))
+ : (List Nat)
+ -> (cons Nat z (nil Nat)))
 
-(check-equal?
- (cons Nat z (nil Nat))
- (list-append Nat (nil Nat) (cons Nat z (nil Nat))))
+(check-type
+ (list-append Nat (cons Nat z (nil Nat)) (cons Nat (s z) (nil Nat)))
+ : (List Nat)
+ -> (cons Nat z (cons Nat (s z) (nil Nat))))
 
-(check-equal?
- (cons Nat z (cons Nat (s z) (nil Nat)))
- (list-append Nat (cons Nat z (nil Nat)) (cons Nat (s z) (nil Nat))))
+(check-type
+ (length Nat (list-append Nat (cons Nat z (nil Nat)) (cons Nat (s z) (nil Nat))))
+ : Nat
+ -> 2)
