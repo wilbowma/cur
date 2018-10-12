f323779a07ed80cb0d44bdbe6d73648822e3ff52
Stephen Chang <stchang@ccs.neu.edu>
2018-10-12 17:18:15 -0400

    cur-test: enable racket-ascii tests

--- cur-test/cur/info.rkt
+++ cur-test/cur/info.rkt
@@ -7,7 +7,6 @@
     "tests/ntac.rkt"
     "tests/olly.rkt"
     "tests/stdlib/ascii.rkt"
-    "tests/stdlib/racket-ascii.rkt"
     "tests/stdlib/sigma.rkt"
     "tests/stdlib/typeclass.rkt"
     "tests/stlc.rkt"))
