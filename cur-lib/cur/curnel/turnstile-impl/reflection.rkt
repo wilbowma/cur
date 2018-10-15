cd0388d6149429a737d253e6826fab7354768ba2
William J. Bowman <wjb@williamjbowman.com>
2018-10-15 18:44:14 -0400

    fix issues with reflection library for turnstile

--- cur-lib/cur/curnel/turnstile-impl/reflection.rkt
+++ cur-lib/cur/curnel/turnstile-impl/reflection.rkt
@@ -1,8 +1,9 @@
 #lang racket/base
 
-(provide (for-syntax cur-expand))
+(provide (for-syntax cur-expand cur-type-infer))
 
 (require (only-in macrotypes/typecheck-core expand1)
+         (only-in turnstile/lang typeof)
          (for-syntax racket/base))
 
 (define-for-syntax (cur-expand stx env) (expand1 stx env))
