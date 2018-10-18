1d18e37ea14609d27dc66caadb4b9f79efb50147
Stephen Chang <stchang@ccs.neu.edu>
2018-10-18 12:14:22 -0400

    cur-test: add rackunit-macrotypes-lib pkg dependency

--- cur-test/info.rkt
+++ cur-test/info.rkt
@@ -1,7 +1,13 @@
 #lang info
 (define collection 'multi)
 (define deps '())
-(define build-deps '("base" "rackunit-lib" ("cur-lib" #:version "0.40") "sweet-exp-lib" "chk-lib"))
+(define build-deps
+  '("base"
+    "rackunit-lib"
+    ("cur-lib" #:version "0.40")
+    "sweet-exp-lib"
+    "chk-lib"
+    "rackunit-macrotypes-lib"))
 (define update-implies '("cur-lib"))
 (define pkg-desc "Tests for \"cur\".")
 (define pkg-authors '(wilbowma))
