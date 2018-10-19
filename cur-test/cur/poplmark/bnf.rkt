e482d3ad5de068d49d3e5aa1662dcab441f51bd8
William J. Bowman <wjb@williamjbowman.com>
2018-10-19 13:33:12 -0400

    Working on BNF parsing

--- cur-test/cur/poplmark/bnf.rkt
+++ cur-test/cur/poplmark/bnf.rkt
@@ -43,8 +43,13 @@ Syntax for BNF grammars in Cur.
                       (local-expand #'x 'expression null)
                       #f)))
 
+  (define-syntax-class cur-term
+    (pattern e:expr #:when (with-handlers ([exn:fail:syntax:unbound? (lambda (_) #f)])
+                             (cur-type-infer #'e))))
+
   (define-syntax-class cur-type
-    (pattern e:expr #:when (cur-type-infer #'e)))
+    (pattern e:expr #:when (with-handlers ([exn:fail:syntax:unbound? (lambda (_) #f)])
+                             (cur-type-infer #'e))))
 
   (require (for-syntax racket/base))
 
@@ -68,125 +73,125 @@ Syntax for BNF grammars in Cur.
   ;; - A Cur Type
   ;; TODO: No recursive structure? Couldn't figure out how to interpret it.
 
-  ;; A BNF-Production is (a syntax object representing a) one of:
-  ;;   - BNF-Expr
-  ;;   - A list starting with a Free-Id followed by BNF-Exprs, i.e.,
-  ;;     (Free-Id BNF-Expr ...)
-  ;;   TODO: Loosen final clause to allow mix fix BNF-Productions?
-  ;;
-  ;; A BNF-Production is parsed into the bnf-production structure, which records
-  ;; a unique constructor name for the Cur representation of the BNF production
-  ;; and the Cur type.
-  (struct bnf-production (ctor type parse))
+  ;; A BNF-Arg-Expr is a BNF-Expr excluding Free-Id
+  ;; - A BNF nonterminal
+  ;; - A Cur Type
 
-  ;; Every production belongs to a nonterminal, which also represents the type of the production.
-  ;; parse-bnf-production takes this bnf-nonterminal, and a BNF-Production, and
-  ;; produces a bnf-production structure.
-  (define (parse-bnf-production nt stx)
-    (syntax-parse stx
-      [(x:free-id e ...)
-       (let ([ctor (syntax/loc stx x)])
-         (bnf-production
-          ctor
-          #`(-> #,@(map (curry parse-bnf-expr-arg nt) (attribute e))
-                #,(bnf-nonterminal-name nt))
-
-          (lambda (f stx)
-            (syntax-parse stx
-              [(y e ...)
-               #:when (free-identifier=? #'y #'x)
-               (quasisyntax/loc stx
-                 (#,ctor
-                  ;; TODO: This is wrong for recursion
-                  #,@(map f (attribute e))))]
-              [_ (f stx)]))))]
-      [_
-       (parse-bnf-expr nt stx)]))
-
-  ;; Parse a BNF-Expr as an argument in a BNF-Production
-  (define (parse-bnf-expr-arg nt stx)
+  ;; Parse a BNF-Arg-Expr as into the right constructor when parsing a BNF-Sexpr-Production
+  (define (parse-bnf-arg-expr nt stx)
     (syntax-parse stx
       [N:bnf-nt (bnf-nonterminal-name (syntax-local-value #'N))]
       [x:free-id
        (bnf-nonterminal-name nt)]
       [e:cur-type stx]))
 
-  ;; Parse a BNF-Expr as a BNF-Production
+  ;; A BNF-Production is (a syntax object representing a) one of:
+  ;;   - BNF-Expr
+  ;;   - A list of BNF-Arg-Exprs with a Free-Id at some position in the list
+  ;;     (BNF-Arg-Expr ... Free-Id BNF-Arg-Expr ...)
+  (define (bnf-production-sexprize nt stx)
+    (if (stx-pair? stx)
+        (let-values ([(ctor args)
+                      (for/fold ([a #f]
+                                 [ls '()])
+                                ([arg (syntax->list stx)])
+                        (syntax-parse arg
+                          ;; NB: Order of these clauses matters
+                          [(~or e:bnf-nt e:cur-type)
+                           (values a (cons #'e ls))]
+                          [x:free-id
+                           #:when (ormap (curry free-identifier=? #'x) (syntax->list (bnf-nonterminal-ids nt)))
+                           (values a (cons #'x ls))]
+                          [x:free-id
+                           (values arg ls)]))])
+          #`(#,ctor #,@(reverse args)))
+        stx))
+
+  ;; A BNF-Sexpr-Production is a BNF-Production but the Free-Id must always
+  ;; appear in head position.
+  ;;   - BNF-Expr
+  ;;   - A list of BNF-Exprs preceeded by a Free-Id
+  ;;     (Free-Id BNF-Arg-Expr ...)
+
+  ;; A BNF-SexprProduction is parsed into the bnf-production structure, which records
+  ;; a unique constructor name for the Cur representation of the BNF production
+  ;; and the Cur type.
+  ;; The Constructor ctor is a canonical identifier for this production.
+  (struct bnf-production (ctor type))
+
+  ;; Every production belongs to a nonterminal, which also represents the type of the production.
+  ;; parse-bnf-production takes this bnf-nonterminal, and a BNF-Production, and
+  ;; produces a bnf-production structure.
+  (define (parse-bnf-production nt stx)
+    (syntax-parse (bnf-production-sexprize nt stx)
+      [(x:free-id e ...)
+       (let ([ctor (syntax/loc stx x)])
+         (bnf-production
+          ctor
+          #`(-> #,@(map (curry parse-bnf-arg-expr nt) (attribute e))
+                #,(bnf-nonterminal-name nt))))]
+      [_
+       (parse-bnf-expr nt stx)]))
+
+    ;; Parse a BNF-Expr as a BNF-Production
   (define (parse-bnf-expr nt stx)
     (syntax-parse stx
       [N:bnf-nt
        (let* ([N-type (bnf-nonterminal-name (syntax-local-value #'N))]
               [nt-type (bnf-nonterminal-name nt)]
               [ctor (format-id stx "~a->~a" N-type nt-type)])
-         (when (free-identifier=? N-type nt-type)
-           (raise-syntax-error "Cannot have unguarded recursive BNF" stx))
          (bnf-production
           ctor
           (quasisyntax/loc stx
-            (-> N-type #,nt-type))
-          (lambda (f stx)
-            (syntax-parse stx
-              [N1:bnf-nt
-               #:when (free-identifier=? #'N #'N1)
-               (quasisyntax/loc stx
-                 (#,ctor N1))]
-              [_ (f stx)]))))]
+            (-> N-type #,nt-type))))]
       [x:free-id
        (bnf-production
         (syntax/loc stx x)
-        (bnf-nonterminal-name nt)
-        (lambda (f stx)
-          (syntax-parse stx
-            [y:free-id
-             (syntax/loc stx y)]
-            [_ (f stx)])))]
+        (bnf-nonterminal-name nt))]
       [A:cur-type
        (let* ([nt-type (bnf-nonterminal-name nt)]
               [ctor (format-id stx "~a->~a" #'A nt-type)])
          (bnf-production
           ctor
           (quasisyntax/loc stx
-            (-> e #,nt-type))
-          (lambda (f stx)
-            (syntax-parse stx
-              [(~of-cur-type e A)
-               (quasisyntax/loc stx
-                 (#,ctor e))]
-              [_ (f stx)]))))]))
+            (-> e #,nt-type))))]))
 
-  (struct bnf-nonterminal (name parser)
+  (struct bnf-nonterminal (name ids)
     #:property prop:procedure
     (lambda (stx)
       (raise-syntax-error
        (format "~a is a reserved name and can only be used in a BNF literal."
                (stx-car stx)))))
-  )
 
-;; TODO: Support unquote
+  ;; TODO: These are not goign to be free-ids anymore, but bound ids, i.e., constructors for nt.
+  ;; define the bnf literal data and rewrite
+  (define (parse-bnf-literal nt stx)
+    (syntax-parse (bnf-production-sexprize nt stx)
+      ;; TODO: Duplicate between this and earlier parsing to constructor ctors
+      [(x:free-id e ...)
+       (quasisyntax/loc stx
+         (x
+          #,@(map parse-bnf-literal (attribute e))))]
+      [x:free-id
+       this-syntax]
+      [e:cur-term
+       (displayln (bnf-nonterminal-name nt))
+       (displayln (cur-type-infer #'e))
+       (let* ([nt-type (bnf-nonterminal-name nt)]
+              [ctor (format-id stx "~a->~a" (cur-type-infer #'e) nt-type)])
+         (quasisyntax/loc stx
+           (#,ctor e)))])))
+
 (define-syntax (bnf stx)
   (syntax-parse stx
     [(_ name:id literal)
-     ((bnf-nonterminal-parser (syntax-local-value #'name)) #'literal)]))
-
-(define-for-syntax (make-bnf-production-parser prods)
-  (lambda (stx ctors)
-    ((for/fold ([f
-                (lambda (f stx)
-                  (f (lambda (stx)
-                       (syntax-parse stx
-                         [_ (raise-syntax-error
-                             (format
-                              "Could not parse BNF expression as ~a"
-                              (syntax->datum #'name))
-                             stx)]))
-                     stx))])
-              ([prod prods])
-       ;; TODO: This is too complicated and doesn't generate a recursive parser.
-      (lambda (f stx)
-        (f (lambda (stx)
-             (bnf-production-parse prod) f stx) stx)))
-     (lambda (f stx) (f stx))
-     stx)))
+     (parse-bnf-literal (syntax-local-value #'name) #'literal)]))
+;;; Here's how bnf should work.
+;;; First, is s-exprizes the literal.
+;;; Next, it walks the tree.
+;;;   Any sexpr whose head is a bnf constructor is left alone at the top-level.
+;;;   Any sexpr whose head is not a bnf constructor must be a cur-type, and is
+;;;   replaced by the appropriate constructor
 
 (define-syntax (define-data/bnf stx)
   (syntax-parse stx
@@ -195,17 +200,14 @@ Syntax for BNF grammars in Cur.
         e
         ...
         ;; TODO: Check that f has the right type?
-        (~optional (~seq #:parser f)))
-     #:do [(define bnf-nt (bnf-nonterminal #'name #f))
+        #;(~optional (~seq #:parser f)))
+     #:do [(define bnf-nt (bnf-nonterminal #'name #'(nt nts ...)))
            (define bnf-prods (map (curry parse-bnf-production bnf-nt) (attribute e)))]
      #:with (c ...) (map bnf-production-ctor bnf-prods)
      #:with (t ...) (map bnf-production-type bnf-prods)
      (quasisyntax/loc this-syntax
        (begin
-         (define-for-syntax (parser stx)
-           (#,(if (attribute f) #'f #`(make-bnf-production-parser (list #,@bnf-prods)))
-            stx (list #'c ...)))
-         (define-syntax nt (bnf-nonterminal #'name parser))
+         (define-syntax nt #,bnf-nt)
          (define-syntax nts (make-variable-like-transformer #'nt)) ...
 
          #,(quasisyntax/loc this-syntax
@@ -221,17 +223,17 @@ Syntax for BNF grammars in Cur.
   z
   (bnf N z)
   (s z)
-  (bnf N (s z))
+  #;(bnf N (s z))
 
   (define-data/bnf List (L) ::= nil (cons N L)
-   ; #:parser (lambda (stx ctors) stx)
+    ; #:parser (lambda (stx ctors) stx)
     )
   nil
   (cons z nil)
   ;(cons nil nil) -> type error
 
 ;  (require (for-syntax racket/dict racket/list syntax/id-table))
-;  (define-data/bnf Arith-Term (e) ::= Nat (+ e e) (- e e) (* e e) error
+;  (define-data/bnf Arith-Term (e) ::= Nat (e + e) (e - e) (e * e) error
 ;    #:parser (lambda (stx ctors)
 ;               (define d
 ;                 (make-immutable-free-id-table
