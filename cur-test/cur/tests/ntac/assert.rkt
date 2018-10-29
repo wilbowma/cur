fcaffa2aa80fea7b7ce4332ab2342f82123a7d8a
Stephen Chang <stchang@ccs.neu.edu>
2018-10-29 17:22:45 -0400

    ntac: fix assert tactic
    - enable tests/ntac/assert test
    - ntac/rewrite: dont use racket-impl

--- cur-test/cur/tests/ntac/assert.rkt
+++ cur-test/cur/tests/ntac/assert.rkt
@@ -5,18 +5,47 @@
          cur/ntac/base
          cur/ntac/standard
          cur/ntac/rewrite
-         rackunit)
+         "../rackunit-ntac.rkt")
 
 ;; basic assert examples
 
-(define-theorem m=n
-  (∀ [x : Nat] [y : Nat]
-     (-> (== Nat x y)
-         (== Nat y x)))
-  (by-intros x y x=y)
-  (by-assert y=x (== Nat y x))
-  ;; proving y=x
-  (by-rewriteR x=y)
-  reflexivity
-  ;; proving goal
-  (by-assumption y=x))
+(check-ntac-trace
+ (∀ [x : Nat] [y : Nat]
+    (-> (== Nat x y)
+        (== Nat y x)))
+ (by-intros x y x=y)
+ (by-assert y=x (== Nat y x))
+ ;; proving y=x
+ (by-rewriteR x=y)
+ reflexivity
+ ;; proving goal
+ (by-assumption y=x)
+ ~>
+ 
+ --------------------------------
+ (Π (x : Nat) (y : Nat) (temp1 : (== Nat x y)) (== Nat y x))
+
+ x : Nat
+ y : Nat
+ x=y : (== Nat x y)
+ --------------------------------
+ (== Nat y x)
+
+ x : Nat
+ y : Nat
+ x=y : (== Nat x y)
+ --------------------------------
+ (== Nat y x)
+
+ x : Nat
+ y : Nat
+ x=y : (== Nat x y)
+ --------------------------------
+ (== Nat y y)
+
+ x : Nat
+ y : Nat
+ x=y : (== Nat x y)
+ y=x : (== Nat y x)
+ --------------------------------
+ (== Nat y x))
