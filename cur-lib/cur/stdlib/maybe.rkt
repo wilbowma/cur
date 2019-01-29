c3912660fc0945f6d8d6c9361c22d60c18ae2fb6
Stephen Chang <stchang@ccs.neu.edu>
2019-01-29 16:35:53 -0500

    stdlib/sugar: define-implicit: new form also accepts explicit args
    - use define-implicit in more places
    - fix bug in expected type case: boundvars should be all Xs

--- cur-lib/cur/stdlib/maybe.rkt
+++ cur-lib/cur/stdlib/maybe.rkt
@@ -1,13 +1,13 @@
 #lang s-exp "../main.rkt"
 (require "sugar.rkt")
-(provide Maybe none (rename-out [some/i some]))
+(provide Maybe (rename-out [none/i none] [some/i some]))
 
 (data Maybe : 1 (Π (A : Type) Type)
   (none : (Π (A : Type) (Maybe A)))
   (some : (Π (A : Type) (Π (a : A) (Maybe A)))))
 
-;; inferring version of some
-; Π  λ ≻ ⊢ ≫ → ∧ (bidir ⇒ ⇐) τ⊑ ⇑
-(define-typed-syntax some/i
-  [(_ A a) ≫ --- [≻ (some A a)]]
-  [(_ a) ≫ [⊢ a ≫ a- ⇒ A] --- [≻ (some A a-)]])
+;; inferring version of some and none
+;; TODO: should define-datatype automatically define these?
+(define-implicit some/i = some 1)
+(define-implicit none/i = none 1)
+
