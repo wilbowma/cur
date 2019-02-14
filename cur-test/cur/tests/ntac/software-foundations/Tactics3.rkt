e167de7346b599438f54bd1392c23cddff7ece4f
Stephen Chang <stchang@ccs.neu.edu>
2019-02-14 11:10:24 -0500

    ntac: inversion properly handles arbitrary base cases

--- cur-test/cur/tests/ntac/software-foundations/Tactics3.rkt
+++ cur-test/cur/tests/ntac/software-foundations/Tactics3.rkt
@@ -60,7 +60,7 @@
   [(S n-1) => (S (S (double n-1)))])
 
 ;; tests inversion of H with non-id base cases
-(define-theorem double_injective
+(define-theorem double-injective
   (forall (n m : nat)
           (-> (== (double n) (double m))
               (== n m)))
@@ -83,7 +83,54 @@
   (by-rewrite H2)
   reflexivity)
 
-(check-type double_injective
+(check-type double-injective
  : (forall (n m : nat)
           (-> (== (double n) (double m))
               (== n m))))
+
+(define-theorem beq-nat-true
+  (∀ [n m : nat]
+     (-> (== (beq-nat n m) true)
+         (== n m)))
+  (by-intro n)
+  (by-induction n #:as [() (n-1 IH)])
+  (by-intros m H) ; 1
+  (by-destruct m #:as [() (m-1)])
+  reflexivity ; 1a
+  (by-inversion H) ; 1b
+  elim-False
+  by-assumption
+  (by-intros m H) ; 2
+  (by-destruct m #:as [() (m-1)])
+  (by-inversion H) ; 2a
+  elim-False
+  by-assumption
+  (by-apply f-equal #:with nat nat S n-1 m-1) ; 2b
+  (by-apply IH)
+  (by-inversion H #:as H1)
+  (by-rewrite H1)
+  reflexivity)
+
+(define-theorem double-injective-take2
+  (∀ [n m : nat]
+     (-> (== (double n) (double m))
+         (== n m)))
+  (by-intros n m)
+  (by-generalize n)
+  (by-induction m #:as [() (m-1 IH)])
+  (by-intros n H) ; 1
+  (by-destruct n #:as [() (n-1)])
+  reflexivity ; 1a
+  (by-inversion H) ; 1b
+  elim-False
+  by-assumption
+  (by-intros n H) ; 2
+  (by-destruct n #:as [() (n-1)])
+  (by-inversion H) ; 2a
+  elim-False
+  by-assumption
+  (by-apply f-equal #:with nat nat S n-1 m-1) ; 2b
+  (by-apply IH)
+  (by-inversion H #:as H1)
+  (by-rewrite H1)
+  reflexivity)
