2c0c41aaf502e51746efd9557f14fc8bbdf7e688
Stephen Chang <stchang@ccs.neu.edu>
2019-05-20 16:44:37 -0400

    stdlib/axiom: add define-extension-type-rule and print-extensions

--- cur-lib/cur/stdlib/axiom.rkt
+++ cur-lib/cur/stdlib/axiom.rkt
@@ -1,8 +1,9 @@
 #lang racket/base
-(provide define-axiom print-assumptions im-an-axiom
+(provide define-axiom print-assumptions im-an-axiom print-extensions define-extension-type-rule
          (for-syntax axiom find-all-axioms))
 
 (require syntax/parse/define
+         racket/pretty
          turnstile/base turnstile/typedefs
          (for-syntax racket/base (only-in racket/list append-map)))
 
@@ -57,3 +58,50 @@
        (printf "Axioms used by \"~s\":\n" 'expr)
        (printf " - ~s : ~s\n" 'axiom-name 'axiom-type)
        ...)])
+
+(define-syntax define-extension-type-rule
+  (syntax-parser
+    [(_ (name . args) rule-part ... [(~datum ⊢) e (~datum ⇒) ty])
+     #'(define-typed-syntax (name . args)
+         rule-part ...
+         [⊢ e (⇒ : ty) (⇒ extension #,this-syntax)
+                       (⇒ extension-name name)])]))
+     
+(begin-for-syntax
+  (define-syntax-class extension
+    #:attributes (e name)
+    [pattern e0
+             #:when (syntax-property #'e0 'extension)
+             #:with e (syntax-property #'e0 'extension)
+             #:with name (syntax-property #'e0 'extension-name)])
+
+  ;; syntax -> (listof (list term-stx type-stx name))
+  (define (find-all-extensions stx)
+    (let find ([extensions null]
+               [stx stx])
+      (syntax-parse stx
+        [a:extension
+         (cons (list #'a.e (resugar-type (typeof stx)) #'a.name) extensions)]
+        [(stuff ...)
+         (for/fold ([ex extensions]) ([stx (in-list (attribute stuff))])
+           (find ex stx))]
+        [_ extensions]))))
+
+(define-syntax-parser print-extensions
+  [(_ expr)
+   #:with [(e ty name) ...] (find-all-extensions (expand/df #'expr))
+   #`(begin
+       (printf "Extensions used by \"~s\":\n" 'expr)
+       #,@(stx-map
+           (λ (e t name)
+             #`(begin
+                 (printf "[~a,~a] via rule ~a:\n"
+                         #,(syntax-line e)
+                         #,(syntax-column e)
+                         '#,name)
+                 (pretty-print '#,(resugar-type e))
+                 (display " : ")
+                 (pretty-print '#,t)))
+           #'(e ...)
+           #'(ty ...)
+           #'(name ...)))])
--- cur-test/cur/tests/stdlib/axiom.rkt
+++ cur-test/cur/tests/stdlib/axiom.rkt
@@ -1,10 +1,12 @@
 #lang cur
 (require
+ cur/stdlib/nat
+ cur/stdlib/bool
  cur/stdlib/sugar
  cur/stdlib/equality
- cur/stdlib/bool
  cur/stdlib/axiom
- (only-in racket/base module))
+ (only-in racket/base module)
+ rackunit/turnstile)
 
 (module check-output racket/base
   (provide check-output)
@@ -50,3 +52,47 @@
  - false=true : (== Bool false true)
 "
  (print-assumptions not-fine))
+
+;; plus-n-0 (no J)
+(check-type
+ (λ [n : Nat]
+   (new-elim
+    n
+    (λ [n : Nat] (== Nat n (plus n 0)))
+    (refl Nat 0)
+    (λ [n-1 : Nat]
+      (λ [IH : (== Nat n-1 (plus n-1 0))]
+        (new-elim
+         IH
+         (λ [m : Nat]
+           (λ [H : (== Nat n-1 m)]
+             (== Nat (s n-1) (s m))))
+         (refl Nat (s n-1)))))))
+ : (∀ [n : Nat] (== Nat n (plus n 0))))
+
+(define-extension-type-rule (J t P pt w peq) ≫
+  [⊢ t ≫ t- ⇒ A]
+  [⊢ P ≫ P- ⇐ (→ A Type)]
+  [⊢ pt ≫ pt- ⇐ (P- t-)]
+  [⊢ w ≫ w- ⇐ A]
+  [⊢ peq ≫ peq- ⇐ (== A t- w-)]
+  --------------
+  [⊢ pt- ⇒ (P- w-)])
+
+;; plus zero (right)
+(define plus0r
+  (λ [n : Nat]
+   (elim-Nat n
+             (λ [m : Nat] (== Nat (plus m 0) m))
+             (refl Nat 0)
+             (λ [k : Nat]
+               (λ [p : (== Nat (plus k 0) k)]
+                 (J (plus k 0)
+                    (λ [W : Nat] (== Nat (s (plus k 0)) (s W)))
+                    (refl Nat (s (plus k 0)))
+                    k
+                    p))))))
+(check-type plus0r : (Π [n : Nat] (== Nat (plus n 0) n)))
+
+(print-extensions plus0r)
+
