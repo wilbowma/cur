575521294ccef1736a2d1eb365209beffaf31da8
William J. Bowman <wjb@williamjbowman.com>
2019-08-09 11:19:18 -0700

    Closes #60

---
+++ cur-test/cur/tests/issue-60.rkt
@@ -0,0 +1,12 @@
+#lang cur
+(require
+ cur/stdlib/sugar
+ cur/stdlib/prop
+ rackunit/turnstile)
+
+(typecheck-fail/toplvl
+ (define-datatype Nat : Type
+       (z : Nat)
+       (s : (-> (-> (-> Nat False) False) Nat)))
+ #:with-msg "does not satisfy strict positivity")
+
