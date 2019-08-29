ad521d0f64d3a35a1c7bcb4360d9efc6ab98190c
Stephen Chang <stchang@ccs.neu.edu>
2019-08-29 11:05:13 -0400

    add test timeout for ntac/sf/Imp-var

--- cur-test/cur/info.rkt
+++ cur-test/cur/info.rkt
@@ -1,6 +1,8 @@
 #lang info
 (define collection 'multi)
 (define compile-omit-paths 'all)
+(define test-timeouts
+  '(("tests/ntac/software-foundations/Imp-var.rkt" 240)))
 (define test-omit-paths
   '("tests/curnel/"
     "tests/olly.rkt"
