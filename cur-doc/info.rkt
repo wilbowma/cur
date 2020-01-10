d6459809ffb6ade43a19dcd8b3998c2405557219
William J. Bowman <wjb@williamjbowman.com>
2020-01-09 16:40:59 -0800

    Add dependency on Racket 7.6

--- cur-doc/info.rkt
+++ cur-doc/info.rkt
@@ -1,7 +1,6 @@
 #lang info
 (define collection 'multi)
 (define deps '("base"))
-;; See README if you want to use in version < 7.0
-(define build-deps '(("base" #:version "7.0") "scribble-lib" "racket-doc" "sandbox-lib" ("cur-lib" #:version "0.5")))
+(define build-deps '(("base" #:version "7.6") "scribble-lib" "racket-doc" "sandbox-lib" ("cur-lib" #:version "0.5")))
 (define pkg-desc "Documentation for \"cur\".")
 (define pkg-authors '(wilbowma))
