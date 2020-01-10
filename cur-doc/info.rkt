246de8e404312f2937ba2a01662ac05bb2c60b85
William J. Bowman <wjb@williamjbowman.com>
2020-01-09 16:41:31 -0800

    Cur version bump to auto

--- cur-doc/info.rkt
+++ cur-doc/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection 'multi)
 (define deps '("base"))
-(define build-deps '(("base" #:version "7.6") "scribble-lib" "racket-doc" "sandbox-lib" ("cur-lib" #:version "0.5")))
+(define build-deps '(("base" #:version "7.6") "scribble-lib" "racket-doc" "sandbox-lib" ("cur-lib" #:version "0.6")))
 (define pkg-desc "Documentation for \"cur\".")
 (define pkg-authors '(wilbowma))
