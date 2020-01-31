755a2d8281adcdfc82b048bd8bb9c3c2d38f7da2
Stephen Chang <stchang@ccs.neu.edu>
2020-01-31 17:11:28 -0500

    extend ntac issue104 with solution using metantac

--- cur-test/cur/tests/ntac/issue104.rkt
+++ cur-test/cur/tests/ntac/issue104.rkt
@@ -4,17 +4,31 @@
  cur/ntac/base
  cur/ntac/standard)
 
+;; example from Sebastian Ullrich, see github issue#104
+
 (begin-for-syntax
   (require syntax/parse/define)
-  (define-simple-macro (mytac)
-    (try (by-intros P p) (by-exact p))))
+  (define-simple-macro (mytac1)
+    (try (by-intros P p) (by-exact p)))
+  (define-simple-macro (mytac2)
+    (seq (by-intros P p) (by-exact p))))
 
-(define-theorem foo
+(require cur/ntac/metantac)
+(begin-for-syntax
+  (define-tactical (mytac3)
+    ((compose (by-exact p) (by-intros P p)) $ptz)))
+
+(define-theorem foo1
   (forall (P : Type) (-> P P))
-  (mytac)
-  ;(by-intros P p)
-  ;(by-exact p)
-)
+  (mytac1))
+(define-theorem foo2
+  (forall (P : Type) (-> P P))
+  (mytac2))
+(define-theorem foo3
+  (forall (P : Type) (-> P P))
+  (mytac3))
 
 (require "rackunit-ntac.rkt")
-(:: foo (forall (P : Type) (-> P P)))
+(:: foo1 (forall (P : Type) (-> P P)))
+(:: foo2 (forall (P : Type) (-> P P)))
+(:: foo3 (forall (P : Type) (-> P P)))
