9453881bf4c1e71ce14415e718cf20ce73f0b8b7
William J. Bowman <wjb@williamjbowman.com>
2020-06-08 23:18:42 -0700

    Update expected error messages in tests

--- cur-test/cur/tests/stdlib/pat.rkt
+++ cur-test/cur/tests/stdlib/pat.rkt
@@ -28,7 +28,7 @@
  (define/rec/match bad-plus : Nat [n : Nat] -> Nat
    [z => n]
    [(s x) => (bad-plus (s x) n)])
- #:with-msg "type mismatch.*s x")
+ #:with-msg "failed termination check")
 
 (define/rec/match bad-minus : Nat Nat -> Nat
    [z _ => z]
@@ -41,7 +41,7 @@
 (typecheck-fail/toplvl
   (define/rec/match bang! : Nat -> (== 0 1)
     [n => (bang! n)])
- #:with-msg "type mismatch.*n")
+  #:with-msg "failed termination check")
 
 (typecheck-fail/toplvl
   (define/rec/match bang!1 : [n : Nat] -> (== 0 1)
@@ -58,7 +58,7 @@
   [z => true]
   [(s z) => false]
   [(s (s x)) => (bad-rec-pat? (s x))])
- #:with-msg "type mismatch.*s x")
+ #:with-msg "failed termination check")
 
 (define/rec/match multi-pat : Nat Nat Nat -> Nat
   [_ _ z => 0]
@@ -87,4 +87,4 @@
 (typecheck-fail/toplvl
  (define/rec/match f : V -> False
    [(cnsv h) => (f (h V (cnsv h)))])
- #:with-msg "type mismatch.*h V")
+ #:with-msg "failed termination check")
