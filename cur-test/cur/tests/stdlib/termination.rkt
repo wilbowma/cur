9b96e49802307fc6e7ee1f993347ee4472101dbb
Stephen Chang <stchang@ccs.neu.edu>
2020-06-12 23:31:33 -0400

    add some termination tests; closes #93 and #94 (#119)

---
+++ cur-test/cur/tests/stdlib/termination.rkt
@@ -0,0 +1,28 @@
+#lang cur
+(require
+  cur/stdlib/nat
+  cur/stdlib/equality
+  cur/stdlib/sugar
+  rackunit/turnstile)
+
+;; examples from @florence
+
+(typecheck-fail/toplvl
+ (define/rec/match bang!1 : [n : Nat] -> (== 0 1)
+   [=> (bang!1 n)])
+ #:with-msg "expected at least one matching variable")
+
+(typecheck-fail/toplvl
+ (define/rec/match bang!2 : -> (== 0 1)
+   [=> (bang!2)])
+ #:with-msg "expected at least one matching variable")
+
+(typecheck-fail/toplvl
+ (define/rec/match bang!3 : Nat -> (== 0 1)
+   [n => (bang!3 n)])
+ #:with-msg "Definition bang!3 failed termination check")
+
+(typecheck-fail/toplvl
+ (define/rec/match bang! : Nat -> (== 0 1)
+   [=> (bang! n)])
+ #:with-msg "expected all match cases to have same number of patterns as number of matching variables")
