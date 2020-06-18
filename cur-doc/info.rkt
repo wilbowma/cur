1f3a883c622b2f858314df2e2caf0249eaa36415
William J. Bowman <wjb@williamjbowman.com>
2020-06-18 14:02:49 -0700

    Fix missing dependency detected by package catalogue

--- cur-doc/info.rkt
+++ cur-doc/info.rkt
@@ -1,6 +1,12 @@
 #lang info
 (define collection 'multi)
 (define deps '("base"))
-(define build-deps '(("base" #:version "7.6") "scribble-lib" "racket-doc" "sandbox-lib" ("cur-lib" #:version "0.6")))
+(define build-deps
+  '(("base" #:version "7.6")
+    "scribble-lib"
+    "racket-doc"
+    "sandbox-lib"
+    ("cur-lib" #:version "0.6")
+    "data-doc"))
 (define pkg-desc "Documentation for \"cur\".")
 (define pkg-authors '(wilbowma))
