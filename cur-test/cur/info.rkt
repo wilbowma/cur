2efd25036f72699de1c1d5f89c6e3d151fee09c5
Stephen Chang <stchang@ccs.neu.edu>
2020-06-30 16:18:30 -0400

    wip issue 134: disabling tests

--- cur-test/cur/info.rkt
+++ cur-test/cur/info.rkt
@@ -7,4 +7,8 @@
   '("tests/curnel/"
     "tests/ntac/interactive.rkt"
     "tests/olly.rkt"
-    "tests/stlc-olly.rkt"))
+    "tests/stlc-olly.rkt"
+    "tests/ntac/induction.rkt"
+    "tests/ntac/inversion.rkt"
+    "tests/ntac/software-foundations/Imp-var.rkt"
+    "tests/ntac/software-foundations/Imp.rkt"))
